77badf5d20f5828049145d3905346f1a6098b522
TimeZero <2925762932@qq.com>
2024-06-12 15:12:31 +0800

    启用vcpkg导入libssh2库

--- ComputersManager/main.cpp
+++ ComputersManager/main.cpp
@@ -3,12 +3,12 @@
 #include <Windows.h>
 #include "sql_service.h"
 #include "login_window.h"
-
-
+#include "libssh2.h"
 
 int main(int argc, char *argv[])
 {
     SetConsoleOutputCP(CP_UTF8);
+
     QApplication a(argc, argv);
     ComputersManager w;
     return a.exec();
